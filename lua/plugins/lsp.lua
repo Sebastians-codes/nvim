16d91f1268bc60faf2d2a46812ee2d1f6404399c
Sebastian <sebastian@sebastians.codes>
2025-10-29 00:20:10 +0100

    cleanup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -89,8 +89,15 @@ return {
           map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
           map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
           map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-          -- Rename is handled by lua/plugins/rename.lua
-          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+          
+          -- Rename with Telescope UI
+          vim.keymap.set('n', '<leader>rn', function()
+            require('utils.rename').rename()
+          end, { buffer = event.buf, desc = 'LSP: [R]e[n]ame' })
+          
+          -- Code actions for normal and visual mode
+          vim.keymap.set({ 'n', 'x' }, '<leader>ca', vim.lsp.buf.code_action, { buffer = event.buf, desc = 'LSP: [C]ode [A]ction' })
+          
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
           local client = vim.lsp.get_client_by_id(event.data.client_id)
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,8 +1,6 @@
 return {
   'echasnovski/mini.nvim',
   config = function()
-    require('mini.ai').setup { n_lines = 500 }
-    require('mini.surround').setup()
     require('mini.move').setup {
       mappings = {
         -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
